894fb2f409ea4d7b8dfa93842cc7b2536bb6ff9b
mohammad <mohammadakhlaghi843@gmail.com>
2024-08-27 22:54:36 +0200

    quiz-app complited

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Quiz from './components/quiz/Quiz';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Quiz/>
+    </div>
+  );
+}
+
+export default App;
